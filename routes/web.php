e879ab99fe3a6c799cac8b5296450eda79923860
Luis Fernando Raga <whary11@gmail.com>
2019-07-12 10:39:48 -0500

    Nombrando la ruta principal para utilizarla en el header...

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,22 @@
 |
 */
 
-Route::get('/', 'IndexController@index');
+Route::get('/', 'IndexController@index')->name('principal');
 Route::get('/paginas-web', 'IndexController@paginaWeb');
-Route::get('/landing-page', 'IndexController@landing');//tiendaOnline
-Route::get('/tiendas-online', 'IndexController@tiendaOnline');//programacion-web
-Route::get('/programacion-web', 'IndexController@programacionWeb');//googleApps
-Route::get('/google-apps', 'IndexController@googleApps');//googleApps
-Route::get('/redes-sociales', 'IndexController@redesSociales');//posicionamientoWeb
-Route::get('/posicionamiento-web', 'IndexController@posicionamientoWeb');//posicionamientoWeb
-Route::get('/marketing-automatizado', 'IndexController@marketingAutomatizado');//posicionamientoWeb
-Route::get('/blog', 'IndexController@blog');//posicionamientoWeb
-Route::get('/contacto', 'IndexController@contacto');//posicionamientoWeb
+Route::get('/landing-page', 'IndexController@landing');
+Route::get('/tiendas-online', 'IndexController@tiendaOnline');
+Route::get('/programacion-web', 'IndexController@programacionWeb');
+Route::get('/google-apps', 'IndexController@googleApps');
+Route::get('/redes-sociales', 'IndexController@redesSociales');
+Route::get('/posicionamiento-web', 'IndexController@posicionamientoWeb');
+Route::get('/marketing-automatizado', 'IndexController@marketingAutomatizado');
+Route::get('/blog', 'IndexController@blog');
+Route::get('/contacto', 'IndexController@contacto');
 
 Route::get('/ticket', 'IndexController@ticket');
-Route::get('/tickets/get', 'TicketController@getTickets')->middleware('auth');//posicionamientoWeb
+Route::get('/tickets/get', 'TicketController@getTickets')->middleware('auth');
 Route::post('/tickets/create', 'TicketController@createTicket');
-Route::get('/tickets/{num}/view', 'TicketController@updateTicket')->middleware('auth');//posicionamientoWeb
+Route::get('/tickets/{num}/view', 'TicketController@updateTicket')->middleware('auth');
 
 
 Auth::routes();
