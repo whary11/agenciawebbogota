ade675ec755ff7d487ffbb36a562832298561b23
Agencia Web Bogotá <agenciawebbogota@gmail.com>
2020-01-04 14:11:26 -0500

    correccion menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -57,7 +57,6 @@
                                 <li><a href="{{url('tiendas-online')}}">Tiendas Online </a></li>
                                 <li><a href="{{url('programacion-web')}}">Programación Web</a></li>
                                 <li><a href="{{url('sitios-web-y-tiendas-online-en-alquiler')}}">Websites en Alquiler</a></li>
-                                <li><a href="{{url('programacion-web')}}">Programación Web</a></li>
                                 <li><a href="{{url('servicios')}}">Servicios Adicionales</a></li>
                             </ul>
                         <li><a>Marketing Digital <i class="fas fa-chevron-down"></i></a>
