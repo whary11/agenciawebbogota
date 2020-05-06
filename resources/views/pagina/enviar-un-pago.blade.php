1046815e4bbd17bdc0847f30ff346192e495b57d
Agencia Web Bogotá <agenciawebbogota@gmail.com>
2020-05-06 13:06:27 -0500

    unminify

--- resources/views/pagina/enviar-un-pago.blade.php
+++ resources/views/pagina/enviar-un-pago.blade.php
@@ -1,1 +1,164 @@
-<!DOCTYPE html><html lang="es"><head><meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta name="Robots" content="index, follow"> <title>Paga en línea con cualquier medio de pago servicios digitales</title> <meta name="description" content="Paga fácilmente tus servicios contratados con Agencia Web Bogotá usando tarjetas débito y crédito, transferencia bancaria, efectivo, paypal, skrill y bitcoin."/><meta name="author" content="Agencia Web Bogotá"><meta property="og:type" content="service"/> <meta property="og:site_name " content="Agencia Web Bogotá"/> <meta property="og:title" content="Paga en línea con cualquier medio de pago servicios digitales"/> <meta property="og:url" content="https://agenciawebbogota.com/sitios-web-y-tiendas-online-en-alquiler"/> <meta property="og:image" content="https://agenciawebbogota.com/images/our-services/pic12.jpg"/> <meta property="og:description" content="Paga fácilmente tus servicios contratados con Agencia Web Bogotá usando tarjetas débito y crédito, transferencia bancaria, efectivo, paypal, skrill y bitcoin."/> <meta name="twitter:card" content="summary_large_image"> <meta name="twitter:site" content="@AgenciaBogota"> <meta name="twitter:creator" content="@AgenciaBogota"> <meta name="twitter:image" content="https://agenciawebbogota.com/images/our-services/pic12.jpg"> <meta name="twitter:image:alt" content="Dueña de negocio empacando producto vendido por Internet"> <meta name="twitter:title" content="Paga en línea con cualquier medio de pago servicios digitales"> <meta name="twitter:description" content="Paga fácilmente tus servicios contratados con Agencia Web Bogotá usando tarjetas débito y crédito, transferencia bancaria, efectivo, paypal, skrill y bitcoin."><link rel="canonical" href="https://agenciawebbogota.com/enviar-un-pago"/><link rel="alternate" hreflang="en_EN" href="https://en.agenciawebbogota.com/make-a-payment"/> <meta name="csrf-token" content="{{csrf_token()}}"> <link rel="icon" href="{{asset('images/favicon.ico')}}" type="image/x-icon"/> <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/favicon.png')}}"/> <link rel="apple-touch-icon" href="images/apple-icon-touch.png"> <link rel="stylesheet" type="text/css" href="{{asset('css/plugins.css')}}"> <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}"> <link rel="stylesheet" type="text/css" href="{{asset('css/templete.css')}}"> <link class="skin" rel="stylesheet" type="text/css" href="{{asset('css/skin-1.css')}}"> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">@include('partials.analytics')</head><body id="bg"> <div class="page-wraper"> @include('partials.header') <div class="page-content bg-white"> <div class="dlab-bnr-inr p-t30 p-b30 no-webp-pagos webp-pagos"> <div class="container"> <div class="dlab-bnr-inr-entry texto-ini"> <h1 class="text-white">Enviar un Pago</h1> <h2 class="text-white"><span class="text-yellow">Paga seguro</span> productos servicios contratados con Agencia Web Bogotá</h2> </div></div></div><div class="content-block"> <div class="section-full content-inner exp-services" style="background-image:url({{asset('images/background/bg1.png')}});"> <div class="container"> <div class="row"> <div class="col-lg-12"> <div class="section-head text-black text-center"> <h2 class="box-title font-30">Paga tus servicios de Diseño, Desarrollo Web y Marketing Digital</h2> <p class="font-weight-600 font-18">Envía un pago a Agencia Web Bogotá por cualquier servicio contratado utilizando los medios de pago disponibles. Puedes realizar pagos de diseño de páginas web, desarrollo de aplicativos, landing pages, desarrollo de tiendas online (e-commerce), renovación de hosting y dominio, campañas de marketing digital y capacitaciones </p><div class="text-center max-w800 m-auto p-t40"> <a href="https://secure.payco.co/checkoutopen/21307" class="site-button bg-azul-oscuro-agencia outline outline-2"><span class="p-lr10">Enviar un Pago</span></a> <div class="p-t20"></div><picture> <source srcset="{{asset('images/pago_seguro.webp')}}" type="image/webp"> <img src="{{asset('images/pago_seguro.png')}}" alt="Medios de pago epayco"> <picture> <div class="p-t30"></div><source srcset="{{asset('images/paypal-logo.webp')}}" type="image/webp"> <img src="{{asset('images/paypal-logo.png')}}" alt="Medios de pago epayco"> <div class="p-t30"></div><span class="texto-pagos">No generamos cargos adicionales por los pagos online. Comunicate con nosotros para obtener tu referencia de pago y el valor a pagar según el servicio contratado</span> </div></picture> </div></div></div></div></div><div class="section-full overlay-primary-dark bg-img-fix bg-azul-oscuro-agencia"> <div class="container"> <div class="row"> <div class="col-lg-5 col-md-5 content-inner chosesus-content text-white"> <h2 class="box-title m-b15 wow fadeInLeft" data-wow-delay="0.2s">Contacta Ahora con Agencia Web Bogotá<span class="bg-primary"></span></h2> </div><div class="col-lg-7 col-md-7 m-b30"> <form class="inquiry-form wow fadeInUp dzForm" data-wow-delay="0.2s" action="script/contact.php"> <div class="dzFormMsg"></div><h3 class="box-title m-t0 m-b10 text-center">¿Requieres asistencia con tus pagos?<span class="bg-primary"></span></h3> <div class="text-center p-t20"> <a href="https://api.whatsapp.com/send?phone=573168785601" class="site-button btn-whatsapp"><i class="fab fa-whatsapp"></i> ¡Pregúntanos por WhatsApp!</a> <a href="https://m.me/agenciawebbogota" class="site-button btn-messenger"><i class="fab fa-facebook-messenger"></i> ¡Háblanos por Messenger!</a> <p class="p-t10 font-16 text-center">Horario de Atención: Lunes a Sábado 8:00 AM a 6:00 PM</p></div><div class="row"> </div></form> </div></div></div></div></div></div>@include('partials.footer') </div><script src="{{asset('js/jquery.min.js')}}"></script> <script src="{{asset('plugins/wow/wow.js')}}"></script> <script src="{{asset('plugins/bootstrap/js/popper.min.js')}}"></script> <script src="{{asset('plugins/bootstrap/js/bootstrap.min.js')}}"></script> <script src="{{asset('plugins/bootstrap-select/bootstrap-select.min.js')}}"></script> <script src="{{asset('plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js')}}"></script> <script src="{{asset('plugins/magnific-popup/magnific-popup.js')}}"></script> <script src="{{asset('plugins/counter/waypoints-min.js')}}"></script> <script src="{{asset('plugins/counter/counterup.min.js')}}"></script> <script src="{{asset('plugins/imagesloaded/imagesloaded.js')}}"></script> <script src="{{asset('plugins/masonry/masonry-3.1.4.js')}}"></script> <script src="{{asset('plugins/masonry/masonry.filter.js')}}"></script> <script src="{{asset('plugins/owl-carousel/owl.carousel.js')}}"></script> <script src="{{asset('plugins/rangeslider/rangeslider.js')}}"></script> <script src="{{asset('js/custom.js')}}"></script> <script src="{{asset('js/dz.carousel.js')}}"></script> <script src="{{asset('plugins/loading/anime.js')}}"></script> <script src="{{asset('plugins/loading/anime-app3.js')}}"></script> <script src='https://www.google.com/recaptcha/api.js'></script> <script src="{{asset('js/dz.ajax.js')}}"></script> <script>$(function (){$('[data-toggle="tooltip"]').tooltip()}) </script></body></html>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="Robots" content="index, follow">
+    <title>Paga en línea con cualquier medio de pago servicios digitales</title>
+    <meta name="description" content="Paga fácilmente tus servicios contratados con Agencia Web Bogotá usando tarjetas débito y crédito, transferencia bancaria, efectivo, paypal, skrill y bitcoin." />
+	<meta name="author" content="Agencia Web Bogotá">
+	<meta property="og:type" content="service" />
+    <meta property="og:site_name " content="Agencia Web Bogotá" />
+    <meta property="og:title" content="Paga en línea con cualquier medio de pago servicios digitales" />
+    <meta property="og:url" content="https://agenciawebbogota.com/sitios-web-y-tiendas-online-en-alquiler" />
+    <meta property="og:image" content="https://agenciawebbogota.com/images/our-services/pic12.jpg" />
+    <meta property="og:description" content="Paga fácilmente tus servicios contratados con Agencia Web Bogotá usando tarjetas débito y crédito, transferencia bancaria, efectivo, paypal, skrill y bitcoin." />
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@AgenciaBogota">
+    <meta name="twitter:creator" content="@AgenciaBogota">
+    <meta name="twitter:image" content="https://agenciawebbogota.com/images/our-services/pic12.jpg">
+    <meta name="twitter:image:alt" content="Dueña de negocio empacando producto vendido por Internet">
+    <meta name="twitter:title" content="Paga en línea con cualquier medio de pago servicios digitales">
+    <meta name="twitter:description" content="Paga fácilmente tus servicios contratados con Agencia Web Bogotá usando tarjetas débito y crédito, transferencia bancaria, efectivo, paypal, skrill y bitcoin.">
+	<link rel="canonical" href="https://agenciawebbogota.com/enviar-un-pago" />
+	<link rel="alternate" hreflang="en_EN" href="https://en.agenciawebbogota.com/make-a-payment"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- FAVICONS ICON -->
+    <link rel="icon" href="{{asset('images/favicon.ico')}}" type="image/x-icon" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/favicon.png')}}" />
+    <link rel="apple-touch-icon" href="images/apple-icon-touch.png">
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/plugins.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/templete.css')}}">
+    <link class="skin" rel="stylesheet" type="text/css" href="{{asset('css/skin-1.css')}}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	@include('partials.analytics')
+</head>
+<body id="bg">
+    <div class="page-wraper">
+
+
+        <!-- header -->
+        @include('partials.header')
+        <!-- header END -->
+        <!-- Content -->
+        <div class="page-content bg-white">
+            <!-- inner page banner -->
+            <div class="dlab-bnr-inr p-t30 p-b30 no-webp-pagos webp-pagos">
+                <div class="container">
+                    <div class="dlab-bnr-inr-entry texto-ini">
+                        <h1 class="text-white">Enviar un Pago</h1>
+                        <h2 class="text-white"><span class="text-yellow">Paga seguro</span> productos servicios contratados con Agencia Web Bogotá</h2>
+                    </div>
+                </div>
+            </div>
+            <!-- inner page banner END -->           
+
+            <div class="content-block">
+                <!-- About Us -->
+                <div class="section-full content-inner exp-services" style="background-image:url({{asset('images/background/bg1.png')}});">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="section-head text-black text-center">
+                                    <h2 class="box-title font-30">Paga tus servicios de Diseño, Desarrollo Web y Marketing Digital</h2>
+                                    <p class="font-weight-600 font-18">Envía un pago a Agencia Web Bogotá por cualquier servicio contratado utilizando los medios de pago disponibles.
+                                        Puedes realizar pagos de diseño de páginas web, desarrollo de aplicativos, landing pages, desarrollo de tiendas online (e-commerce), renovación de hosting y dominio, 
+                                        campañas de marketing digital y capacitaciones
+                                    </p>
+                                    <div class="text-center max-w800 m-auto p-t40">
+                                        <a href="https://secure.payco.co/checkoutopen/21307" class="site-button bg-azul-oscuro-agencia outline outline-2"><span class="p-lr10">Enviar un Pago</span></a>
+                                        <div class="p-t20"></div>
+                                        <picture>
+                                        <source srcset="{{asset('images/pago_seguro.webp')}}" type="image/webp">
+                                        <img src="{{asset('images/pago_seguro.png')}}" alt="Medios de pago epayco">
+                                        <picture>
+                                        <div class="p-t30"></div>
+                                        <source srcset="{{asset('images/paypal-logo.webp')}}" type="image/webp">
+                                        <img src="{{asset('images/paypal-logo.png')}}" alt="Medios de pago epayco">
+                                        <div class="p-t30"></div>
+                                        <span class="texto-pagos">No generamos cargos adicionales por los pagos online. Comunicate con nosotros para obtener tu referencia de pago y el valor a pagar según el servicio contratado</span>
+                                    </div>
+                                </picture>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Our Services -->              
+
+                <!-- Get in touch -->
+                <div class="section-full overlay-primary-dark bg-img-fix bg-azul-oscuro-agencia">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-5 col-md-5 content-inner chosesus-content text-white">
+                                <h2 class="box-title m-b15 wow fadeInLeft" data-wow-delay="0.2s">Contacta Ahora con
+                                    Agencia Web Bogotá<span class="bg-primary"></span></h2>
+                            </div>
+                            <div class="col-lg-7 col-md-7 m-b30">
+                                <form class="inquiry-form wow fadeInUp dzForm" data-wow-delay="0.2s"
+                                    action="script/contact.php">
+                                    <div class="dzFormMsg"></div>
+                                    <h3 class="box-title m-t0 m-b10 text-center">¿Requieres asistencia con tus pagos?<span class="bg-primary"></span></h3>
+                                    <div class="text-center p-t20">
+                                        <a href="https://api.whatsapp.com/send?phone=573168785601"
+                                            class="site-button btn-whatsapp"><i class="fab fa-whatsapp"></i>
+                                            ¡Pregúntanos por WhatsApp!</a>
+                                        <a href="https://m.me/agenciawebbogota" class="site-button btn-messenger"><i
+                                                class="fab fa-facebook-messenger"></i> ¡Háblanos por Messenger!</a>
+                                        <p class="p-t10 font-16 text-center">Horario de Atención: Lunes a Sábado 8:00 AM
+                                            a 6:00 PM</p>
+                                    </div>
+                                    <div class="row">
+
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Get in touch -->
+            </div>
+            <!-- contact area END -->
+        </div>
+        <!-- Content END-->
+        <!-- Footer -->
+        @include('partials.footer')
+        <!-- Footer END-->
+    </div>
+    <!-- JAVASCRIPT FILES ========================================= -->
+    <script src="{{asset('js/jquery.min.js')}}"></script><!-- JQUERY.MIN JS -->
+    <script src="{{asset('plugins/wow/wow.js')}}"></script><!-- WOW JS -->
+    <script src="{{asset('plugins/bootstrap/js/popper.min.js')}}"></script><!-- BOOTSTRAP.MIN JS -->
+    <script src="{{asset('plugins/bootstrap/js/bootstrap.min.js')}}"></script><!-- BOOTSTRAP.MIN JS -->
+    <script src="{{asset('plugins/bootstrap-select/bootstrap-select.min.js')}}"></script><!-- FORM JS -->
+    <script src="{{asset('plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js')}}"></script><!-- FORM JS -->
+    <script src="{{asset('plugins/magnific-popup/magnific-popup.js')}}"></script><!-- MAGNIFIC POPUP JS -->
+    <script src="{{asset('plugins/counter/waypoints-min.js')}}"></script><!-- WAYPOINTS JS -->
+    <script src="{{asset('plugins/counter/counterup.min.js')}}"></script><!-- COUNTERUP JS -->
+    <script src="{{asset('plugins/imagesloaded/imagesloaded.js')}}"></script><!-- IMAGESLOADED -->
+    <script src="{{asset('plugins/masonry/masonry-3.1.4.js')}}"></script><!-- MASONRY -->
+    <script src="{{asset('plugins/masonry/masonry.filter.js')}}"></script><!-- MASONRY -->
+    <script src="{{asset('plugins/owl-carousel/owl.carousel.js')}}"></script><!-- OWL SLIDER -->
+    <script src="{{asset('plugins/rangeslider/rangeslider.js')}}"></script><!-- Rangeslider -->
+    <script src="{{asset('js/custom.js')}}"></script><!-- CUSTOM FUCTIONS  -->
+    <script src="{{asset('js/dz.carousel.js')}}"></script><!-- SORTCODE FUCTIONS  -->
+    <script src="{{asset('plugins/loading/anime.js')}}"></script><!-- LOADING JS -->
+    <script src="{{asset('plugins/loading/anime-app3.js')}}"></script><!-- LOADING JS -->
+    <script src='https://www.google.com/recaptcha/api.js'></script> <!-- Google API For Recaptcha  -->
+    <script src="{{asset('js/dz.ajax.js')}}"></script><!-- CONTACT JS  -->
+    <script>
+        $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        })
+    </script>
+
+</body>
+
+</html>
